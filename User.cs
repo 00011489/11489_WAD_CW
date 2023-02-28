554cd7856375b481ebd325e61768a98b937d635d
kkholmatov <kkholmatov@exadel.com>
2023-02-28 23:09:26 +0500

    initial codes entity classes, controllers, dbContext added

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class User
+{
+	public User()
+	{
+		public int ID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
